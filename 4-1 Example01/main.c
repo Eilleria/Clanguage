d3cf9724b4d705ad521f8fe55283866566186c87
Eilleria <chlgmwlwo3861@naver.com>
2021-12-17 09:55:48 +0900

    4-1 Example

---
+++ 4-1 Example01/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int a, b;
+	a = 10;
+	b = 3;
+
+	printf("a + b = %d \n", a + b);
+	printf("a - b = %d \n", a - b);
+	printf("a * b = %d \n", a * b);
+	printf("a / b = %d \n", a / b);
+	printf("a %% b = %d \n", a % b);
+
+	return 0;
+}
